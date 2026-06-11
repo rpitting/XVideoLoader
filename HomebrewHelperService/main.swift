26fde4307d13dfcd131fbf76edfa55c9826ecadc
Reiner Pittinger <rp@digital-wave.de>
2026-06-11 21:54:47 +0200

    Set organization name to "digital wave" in copyright headers

--- HomebrewHelperService/main.swift
+++ HomebrewHelperService/main.swift
@@ -3,7 +3,7 @@
 //  XVideoLoader
 //  
 //  Created by Reiner Pittinger on 13.04.26
-//  Copyright © 2026 ___ORGANIZATIONNAME___. All rights reserved.
+//  Copyright © 2026 digital wave. All rights reserved.
     
 
 import Foundation
